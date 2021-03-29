25aa99f179de4e73b30d2814d9a20edb6a12a0c9
juancaruizc <juancruizcruz@gmail.com>
2021-03-29 11:15:04 -0700

    setup complete

---
+++ api/server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const helmet = require('helmet');
+const cors = require('cors');
+const morgan = require('morgan');
+const authRouter = require('./auth/auth-router.js');
+const plantsRouter = require('./plants/plants-router');
+const usersRouter = require('./users/users-router');
+
+const server = express();
+server.use(helmet());
+server.use(cors());
+server.use(morgan('dev'));
+server.use(express.json());
+server.use('/api/auth', authRouter);
+server.use('/api/plants', plantsRouter);
+server.use('/api/users', usersRouter);
+
+server.get('/', (res) => {
+  res.json(`Welcome to the water my plants API`);
+});
+
+module.exports = server;
