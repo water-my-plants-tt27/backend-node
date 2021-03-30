55c3b2228def7a742ddae2b4357fa0d6afa09403
MRSTOP0\Stop0 <dawodmohmmed@gmail.com>
2021-03-29 22:17:20 -0400

    bugs fixed

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -1,73 +1,84 @@
-const router = require('express').Router();
 const bcryptjs = require("bcryptjs");
 const jwt = require('jsonwebtoken');
 
-const Users = require("../users/user-model");
+const router = require("express").Router();
 
-router.post("/register", (req, res) => {
-    const credentials = req.body;
-  
-    if (isValid(credentials)) {
-      const rounds = process.env.BCRYPT_ROUNDS || 8;
-  
-      // hash the password
-      const hash = bcryptjs.hashSync(credentials.password, rounds);
-  
-      credentials.password = hash;
-  
-      // save the user to the database
-      Users.add(credentials)
-        .then(user => {
-          res.status(201).json({ data: user });
-        })
-        .catch(error => {
-          res.status(500).json({ message: error.message });
-        });
-    } else {
-      res.status(400).json({
-        message: "please provide name and password and the password shoud be alphanumeric",
+const { jwtSecret } = require('../../config/secrets');
+
+const Users = require("../users/user-model");
+const { isValid } = require("../users/users-services.js");
+const { checkPayload } = require('./authMiddleware');
+const { checkEmailUnique } = require('./authMiddleware');
+
+
+
+router.post("/register", checkPayload, checkEmailUnique, (req, res) => {
+  const credentials = req.body;
+  if (isValid(credentials)) {
+
+
+    const rounds = process.env.BCRYPT_ROUNDS || 11;
+
+    // hash the password
+    const hash = bcryptjs.hashSync(credentials.password, rounds);
+
+    credentials.password = hash;
+
+    // save the user to the database
+    Users.add(credentials)
+      .then(user => {
+        res.status(201).json({ data: user, message: "registered " });
+      })
+      .catch(err => {
+        res.status(500).json({ message: err.message });
       });
-    }
-  });
-  
-  router.post("/login", (req, res) => {
-    const { email, password } = req.body;
-  
-    if (isValid(req.body)) {
-      Users.getBy({ email: email })
-        .then(([user]) => {
-          // compare the password the hash stored in the database
-          if (user && bcryptjs.compareSync(password, user.password)) {
-            // issue token
-            const token = buildToken(user)
-            res.status(200).json({ message: "Welcome to our API", token });
-          } else {
-            res.status(401).json({ message: "Invalid credentials" });
-          }
-        })
-        .catch(error => {
-          res.status(500).json({ message: error.message });
-        });
-    } else {
-      res.status(400).json({
-        message: "please provide name and password and the password shoud be alphanumeric",
-      });
-    }
-  });
-  
-  function buildToken(user) {
-    const payload = {
-      // claims
-      user_id: user.id,
-      email: user.name,
-    }
-    const config = {
-      expiresIn: '1d',
-    }
-    return jwt.sign(
-      payload, jwtSecret, config
-    )
+  } else {
+    res.status(400).json({
+      message: "please provide username and password",
+    });
   }
-  
+});
+
+router.post("/login", (req, res) => {
+  const { email, password } = req.body;
+
+  if (isValid(req.body)) {
+    Users.getBy({ email: email })
+    //why is it in this array?
+      .then(([user]) => {
+        // compare the password the hash stored in the database
+        if (user && bcryptjs.compareSync(password, user.password)) {
+          const token = makeToken(user)
+          res.status(200).json({             
+          message: `Hello, ${user.name}`,
+          name: user.name,
+          email: user.email,
+          phone_number: user.phone_number,
+          token, });
+        } else {
+          res.status(401).json({ message: "try again" });
+        }
+      })
+      .catch(error => {
+        res.status(500).json({ message: error.message });
+      });
+  } else {
+    res.status(400).json({
+      message: "please provide email and password ",
+    });
+  }
+});
+
+function makeToken(user) {
+  const payload = {
+    subject: user.id,
+    email: user.email,
+  };
+  const options = {
+    expiresIn: '1h',
+  };
+  return jwt.sign(payload, jwtSecret, options);
+}
+
 
 module.exports = router;
--- api/auth/authMiddleware.js
+++ api/auth/authMiddleware.js
@@ -1,19 +1,48 @@
-const jwt = require('jsonwebtoken')
-const { jwtSecret } = require('../../config/secrets')
+const jwt = require('jsonwebtoken');
+const { jwtSecret } = require('../../config/secrets');
 
 module.exports = (req, res, next) => {
-  const token = req.headers.authorization
-
+  const token = req.headers.authorization;
   if (!token) {
-    res.status(401).json({ message: 'Token does not exist' })
+    res.status(401).json('token required');
   } else {
     jwt.verify(token, jwtSecret, (err, decoded) => {
       if (err) {
-        res.status(401).json({ message: 'token is not verified ' })
+        res.status(401).json('token is not verified ');
       } else {
-        req.decodedJwt = decoded
-        next()
+        req.decodedToken = decoded;
+        next();
       }
-    })
+    });
   }
+};
+
+const User = require('../users/user-model');
+
+const checkEmailUnique = async (req, res, next) => {
+  try {
+    const rows = await User.getBy({ email: req.body.email });
+    if (!rows.length) {
+      next();
+    } else {
+      res.status(401).json('email taken');
+    }
+  } catch (err) {
+    res.status(500).json(err.message);
+  }
+};
+
+const checkPayload = (req, res, next) => {
+  if (!req.body.email || !req.body.password) {
+    console.log(req.body)
+    res.status(401).json('Please, email and password required');
+  } else {
+    next();
+  }
+};
+
+
+module.exports = {
+  checkEmailUnique,
+  checkPayload
 };
