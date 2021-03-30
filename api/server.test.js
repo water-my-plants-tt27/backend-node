a8c378286d186278079189efe939f979871775ed
katieolson84 <katieolson84@gmail.com>
2021-03-30 15:26:35 -0600

    login tests

--- api/server.test.js
+++ api/server.test.js
@@ -41,16 +41,31 @@ beforeAll(async () => {
       it('responds with the correct message when username or password are missing', async () => {
         const res = await request(server).post('/api/auth/login')
         .send({ email: 'juan@juan.com'})
-        expect(res.body.message).toMatch('please provide email and password')
+        expect(res.body.message).toMatch('Email and password are required!')
       }, 500)
 
       it('can find a user by the id ', async () => {
-        await db('users').insert({email: "juan@juan.com", password: "1234"})
-        const juan= await User.getById(1)
-        expect(juan).toMatchObject({ id: 1, email: "juan@juan.com"})
+        await db('users')
+        const user= await User.getById(1)
+        expect(user).toMatchObject({ user_id: 1, name: "juan", email: "juan@juan.com", phone_number: "5205551234"})
       })
-
     })
+
+    describe('[GET] /api/users', () => {
+      it('responds with the proper status code and message on not-logged-in user', async () => {
+        const res = await request(server).get('/api/users')
+        expect(res.status).toBe(401)
+        expect(res.body.message).toMatch('token required')
+      }, 500)
+
+      it('shows error message of "invalid credentials" if not verified', async () => {
+        await db('users').insert({email: "katie@kt.com", password: "1234"})
+        let res = await request(server).post('/api/auth/login')
+        .send({ email: 'katie@kt.com', password: '1234' })
+        expect(res.body.message).toMatch('invalid credentials')
+      })
+    })
+
 
 
     })
