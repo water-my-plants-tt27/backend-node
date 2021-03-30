d71f0b9724a0fbd65e4aa4dc9f76dd48ca1db9ee
katieolson84 <katieolson84@gmail.com>
2021-03-30 10:58:45 -0600

    created some testing

---
+++ api/server.test.js
@@ -0,0 +1,56 @@
+const server = require('./server');
+const request = require('supertest');
+const db = require('../database/dbConfig');
+const bcrypt = require('bcryptjs');
+const User = require('./users/user-model')
+
+beforeAll(async () => {
+    await db.migrate.rollback()
+    await db.migrate.latest()
+  })
+  beforeEach(async () => {
+    await db('my_plants').truncate()
+  })
+  afterAll(async () => {
+    await db.destroy()
+  })
+  
+  it('process.env.NODE_ENV must be "testing"', () => {
+    expect(process.env.NODE_ENV).toBe('testing')
+  })
+
+  describe('users endpoints', () => {
+    describe('[POST] /api/auth/register',() => {
+      it('creates a new user in the db', async ()=> {
+        await request(server).post('/api/auth/register')
+        .send({name: 'juan', email: 'juan@juan.com', phone_number: '5205551234', password: 'abc123'})
+        const juan = await db('users')
+        .where('name', 'juan').first()
+        expect(juan).toMatchObject({name: 'juan'})
+      }), 500
+
+      it('new user password is bcrypted', async () => {
+        await request(server).post('/api/auth/register')
+        .send({name: 'juan', email: 'juan@juan.com', phone_number: '5205551234', password: 'abc123'})
+        const juan = await db('users')
+        .where('name', 'juan').first()
+        expect(bcrypt.compareSync('abc123', juan.password)).toBeTruthy()
+      })
+    })
+    describe('[POST] /api/auth/login', () => {
+      it('responds with the correct message when username or password are missing', async () => {
+        const res = await request(server).post('/api/auth/login')
+        .send({ email: 'juan@juan.com'})
+        expect(res.body.message).toMatch('please provide email and password')
+      }, 500)
+
+      it('can find a user by the id ', async () => {
+        await db('users').insert({email: "juan@juan.com", password: "1234"})
+        const juan= await User.getById(1)
+        expect(juan).toMatchObject({ id: 1, email: "juan@juan.com"})
+      })
+
+    })
+
+
+    })
--- api/users/users-router.js
+++ api/users/users-router.js
@@ -1,8 +1,9 @@
 const router = require('express').Router();
 const Users = require('./user-model');
 
-router.get('/', (req,res) => {
-    res.status(200).json(Users);
-})
-
+router.get('/', (req, res, next) => {
+    Users.get()
+      .then((users) => res.status(200).json(users))
+      .catch(next);
+  });
 module.exports = router;
